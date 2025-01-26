8c539321206529f4d77888e5d6b9d0499e023d4a
lla-dane <abhinavagarwalla6@gmail.com>
2025-01-26 22:23:16 +0530

    done with auction logic

--- contracts/bids.sol
+++ contracts/bids.sol
@@ -37,11 +37,16 @@ contract BlindAuction is SepoliaZamaFHEVMConfig, SepoliaZamaGatewayConfig, Gatew
         uint64 tokenCount;
     }
 
-    mapping(address => mapping(address => Bid)) public bids;
-    mapping(address => Auction) public auctions;
-    mapping(address => address[]) public auctionBidders;
-    Auction[] public allAuctions;
-    Bid[] public allBids;
+    mapping(address => mapping(address => Bid)) public bids; //             drop after revealAuction for _auctionId           --
+    mapping(address => Auction) public auctions; //                         drop after revealAuction for _auctionId           --
+    mapping(address => address[]) public auctionBidders; //                 drop after revealAuction for _auctionId           --
+    Auction[] public allAuctions; //                                        drop after claimLeftAuctionStake for _auctionId   --
+    Bid[] public allBids; //                                                for testing purposes                              --
+    mapping(address => Bid[]) winningBids; //                               drop in claimLeftAuctionStake by bidId            --
+    mapping(address => uint256) leftAuctionStake; //                        drop one by one in claimLeftAuctionStake          --
+    mapping(address => mapping(address => uint256)) wonAuctionPrize; //     drop one by one in claimWonAuctionPrize           --
+    mapping(address => mapping(address => uint256)) lostAuctionStake; //    drop one by one in claimLostAuctionStake          --
+    mapping(address => Bid[]) myBids;
 
     // -------------TESTING---------------
 
@@ -77,32 +82,36 @@ contract BlindAuction is SepoliaZamaFHEVMConfig, SepoliaZamaGatewayConfig, Gatew
         return auctions[msg.sender].auctionId == msg.sender;
     }
 
-    // function sortBids(Bid[] memory relBids) internal pure returns (Bid[] memory) {
-    //     uint256 n = relBids.length;
+    function getMyBids() public view returns (Bid[] memory) {
+        return myBids[msg.sender];
+    }
 
-    //     for (uint256 i = 0; i < n - 1; i++) {
-    //         for (uint256 j = 0; j < n - i - 1; j++) {
-    //             if (relBids[j].perTokenRate < relBids[j + 1].perTokenRate) {
-    //                 // Swap relBids[j] and relBids[j + 1]
-    //                 Bid memory temp = relBids[j];
-    //                 relBids[j] = relBids[j + 1];
-    //                 relBids[j + 1] = temp;
-    //             }
-    //         }
-    //     }
-    //     return relBids;
-    // }
+    function sortBids(Bid[] memory relBids) internal pure returns (Bid[] memory) {
+        uint256 n = relBids.length;
 
-    // function getBidsForAuction(address _auctionId) public view returns (Bid[] memory) {
-    //     address[] memory bidders = auctionBidders[_auctionId];
-    //     Bid[] memory auctionBids = new Bid[](bidders.length);
+        for (uint256 i = 0; i < n - 1; i++) {
+            for (uint256 j = 0; j < n - i - 1; j++) {
+                if (relBids[j].perTokenRate < relBids[j + 1].perTokenRate) {
+                    // Swap relBids[j] and relBids[j + 1]
+                    Bid memory temp = relBids[j];
+                    relBids[j] = relBids[j + 1];
+                    relBids[j + 1] = temp;
+                }
+            }
+        }
+        return relBids;
+    }
 
-    //     for (uint256 i = 0; i < bidders.length; i++) {
-    //         auctionBids[i] = bids[_auctionId][bidders[i]];
-    //     }
+    function getBidsForAuction(address _auctionId) public view returns (Bid[] memory) {
+        address[] memory bidders = auctionBidders[_auctionId];
+        Bid[] memory auctionBids = new Bid[](bidders.length);
 
-    //     return sortBids(auctionBids);
-    // }
+        for (uint256 i = 0; i < bidders.length; i++) {
+            auctionBids[i] = bids[_auctionId][bidders[i]];
+        }
+
+        return sortBids(auctionBids);
+    }
 
     // --------------MAJORS------------------
 
@@ -171,26 +180,89 @@ contract BlindAuction is SepoliaZamaFHEVMConfig, SepoliaZamaGatewayConfig, Gatew
         bids[_auctionId][msg.sender] = newBid;
         auctionBidders[_auctionId].push(msg.sender);
         allBids.push(newBid);
+
+        // TODO! Transfer funds of the bid, new ERC20 Token2
+
         return true;
     }
 
-    // function revealAuction() public view returns (Bid[] memory) {
-    //     require((auctions[msg.sender].auctionId == msg.sender) && (auctions[msg.sender].endTime < block.timestamp));
+    function revealAuction(address _auctionId) public returns (Bid[] memory) {
+        require((auctions[msg.sender].auctionId == msg.sender) && (auctions[msg.sender].endTime < block.timestamp));
 
-    //     Bid[] memory relBids = getBidsForAuction((msg.sender));
-    //     uint256 tokenCount = auctions[msg.sender].tokenCount;
-    //     Bid[] memory winningBids = new Bid[](relBids.length);
+        require(auctions[_auctionId].auctionId == msg.sender);
+        require(auctions[_auctionId].endTime < block.timestamp);
 
-    //     for (uint256 i = 0; i < relBids.length; i++) {
-    //         if (tokenCount < relBids[i].tokenCount) {
-    //             winningBids[i] = relBids[i];
-    //             break;
-    //         }
+        Bid[] memory relBids = getBidsForAuction(msg.sender);
+        uint256 tokenCount = auctions[msg.sender].tokenCount;
+        Bid[] memory _winningBids = new Bid[](relBids.length);
 
-    //         winningBids[i] = relBids[i];
-    //         tokenCount -= relBids[i].tokenCount;
-    //     }
+        for (uint256 i = 0; i < relBids.length; i++) {
+            if (0 < tokenCount) {
+                // Winning Bids
+                if (tokenCount > relBids[i].tokenCount) {
+                    _winningBids[i] = relBids[i];
+                    wonAuctionPrize[_auctionId][relBids[i].bidId] = relBids[i].tokenCount;
+                    tokenCount -= relBids[i].tokenCount;
+                } else {
+                    // Partial Win
+                    _winningBids[i] = relBids[i];
+                    wonAuctionPrize[_auctionId][relBids[i].bidId] = tokenCount;
+                    tokenCount = 0;
+                }
+            } else {
+                // Losing Bids
+                lostAuctionStake[_auctionId][relBids[i].bidId] = relBids[i].tokenCount;
+            }
+        }
 
-    //     return winningBids;
-    // }
+        if (tokenCount > 0) {
+            leftAuctionStake[_auctionId] = tokenCount;
+        }
+
+        winningBids[_auctionId] = _winningBids;
+        return _winningBids;
+    }
+
+    function claimWonAuctionPrize(address _auctionId) public {
+        require(wonAuctionPrize[_auctionId][msg.sender] > 0, "Sender is a fraud");
+
+        uint256 _prizeAmount = wonAuctionPrize[_auctionId][msg.sender];
+        delete (wonAuctionPrize[_auctionId][msg.sender]);
+
+        // TODO! Transfer this amount to the auction Winner
+    }
+
+    function claimLeftAuctionStake() public {
+        require(leftAuctionStake[msg.sender] > 0, "You don't own any auction");
+
+        uint256 _redeemAmount = leftAuctionStake[msg.sender];
+
+        // Delete all the data about this auction
+        delete (leftAuctionStake[msg.sender]);
+        delete (winningBids[msg.sender]);
+        delete (auctions[msg.sender]);
+        address[] memory bidders = auctionBidders[msg.sender];
+        for (uint256 i = 0; i < bidders.length; i++) {
+            delete bids[msg.sender][bidders[i]];
+        }
+        delete auctionBidders[msg.sender];
+
+        for (uint256 i = 0; i < allAuctions.length; i++) {
+            if (allAuctions[i].auctionId == msg.sender) {
+                allAuctions[i] = allAuctions[allAuctions.length - 1];
+                allAuctions.pop();
+            }
+        }
+
+        // TODO! Transfer this amount to the auction Owner
+    }
+
+    function claimLostAuctionStake(address _auctionId) public {
+        require(lostAuctionStake[_auctionId][msg.sender] > 0, "Sender is a fraud");
+
+        uint256 _prizeAmount = lostAuctionStake[_auctionId][msg.sender];
+        delete (lostAuctionStake[_auctionId][msg.sender]);
+
+        // TODO! Transfer this amount to the auction Loser
+    }
 }
